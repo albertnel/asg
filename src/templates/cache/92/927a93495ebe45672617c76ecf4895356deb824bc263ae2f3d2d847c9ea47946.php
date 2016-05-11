be5bbea68ca77045c41bc2c0f8cadc17088df9f9
Albert Nel <albert@spiraleye.com>
2016-05-11 23:14:16 +0200

    First commit

---
+++ src/templates/cache/92/927a93495ebe45672617c76ecf4895356deb824bc263ae2f3d2d847c9ea47946.php
@@ -0,0 +1,34 @@
+<?php
+
+/* index.html */
+class __TwigTemplate_a30778517940a5760940e26d86044e3c407cbaaa0389222fab0d49819545e012 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "Index, hello!
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "index.html";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+}
+/* Index, hello!*/
+/* */
