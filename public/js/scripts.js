68b38a7c5cbfc887bd1622f0f8fa09e3227938c5
Albert Nel <albert@spiraleye.com>
2016-05-13 23:01:06 +0200

    Finished add and delete
    * Adds alert message.
    * Catches exceptions.

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,14 @@
 $("#back_button").click(function() {
     window.location = "/";
 });
+
+function deleteContact(id)
+{
+    $.ajax({
+        type: "DELETE",
+        url: "/manage?id=" + id,
+        success: function(msg) {
+            window.location = "/";
+        }
+    });
+}
